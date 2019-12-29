712a6bf243c999471bb541db7917a1e2d1104a3c
Anders Demant van der Weide <antra@antra.dk>
2019-12-29 10:34:13 +0100

    First working version

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # PyTeslaPowerSet
-Check current power prices and set a Tesla charge state accordingly
+Check current power prices and set a Tesla charge state accordingly:
+- If the Tesla is in Trip Mode™ (charge target above 90%) then do nothing
+- If the power price tonight is low, then set the Tesla to charge full
+- If the power price tonight is high, then set the Tesla to charge minimum
 
 ## Thanks!
 Relies on Greg Glockner's [Tesla Json](https://github.com/gglockner/teslajson) and Kimmo Huoman's [Nordpool](https://github.com/kipe/nordpool)  
@@ -22,5 +25,11 @@ TESLA_USER=your_email
 TESLA_PASS=your_password
 TESLA_TOKEN=your_access_token
 ```
-
 If you already have an access token that can be used to avoid storing the credentials locally
+
+There are also a couple of behaviour toggles early in the script, set them according to your need:  
+- `base_currency` - the currency you want to use
+- `areas` - the Nordpool area to check for, NB *currently only supports 1 area*
+- `cheap_threshold` - the decision threshold for whether to charge min or max (e.g. 280), NB *multiply by 1000 (conversion of cost/MWh to cost/kWh)*
+- `min_percent` - the charge percent to use when price is above cheap_threshold (e.g. 60)
+- `max_percent` - the charge percent to use when price is below cheap_threshold (e.g. 90)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-import json
 import os
 from datetime import datetime, timezone, timedelta
 import teslajson
@@ -16,93 +15,97 @@ work1_lat = os.getenv("WORK1_LAT")
 work1_long = os.getenv("WORK1_LONG")
 work2_lat = os.getenv("WORK2_LAT")
 work2_long = os.getenv("WORK2_LONG")
-cheap_power = 28.0
 
-# Then get the power price
-prices_spot = elspot.Prices()
-# Get the most recent prices - and store the first and last values as tonight/tmorrow's prices
-# If this runs too early in the day, the prices may not be set yet (will be 'inf') -- seems the price is announced around 1pm CET
-# TODO: Add handling for prices being 'inf' and check for today instead?
-price_latest24 = prices_spot.hourly(areas=['DK2'])['areas']['DK2']['values']
-price_latest24_earliest, price_latest24_earliest_time = price_latest24[
-    0]['value'], price_latest24[0]['start']
-price_latest24_latest, price_latest24_latest_time = price_latest24[-1]['value'], price_latest24[-1]['start']
+base_currency = 'DKK'
+areas = ['DK2']  # currently only supports 1 area
+# Nordpool prices are per currency/MWh where household expenses are normally currency/kWh - so set threshold as 1000x
+cheap_threshold = 280
+# Thresholds for desired minimum/maximum when in normal usage (not in Trip Mode)
+min_percent = 60
+max_percent = 90
 
-# The price returned is actually EUR/MWh - but that seems to coincide well with the actual electricity kWh-price in DKK (as øre value)
-print("The price tonight", price_latest24_earliest_time,
-      "is:", price_latest24_earliest)
-print("The price tomorrow night", price_latest24_latest_time,
-      "is:", price_latest24_latest)
 # Nordpool module returns times as UTC, generate a 'now' to compare with
+# If this runs too early in the day, the prices may not be set for tomorrow yet (will be 'inf' as float) -- seems the price is announced around 1pm CET
 now = datetime.now(tz=timezone.utc)
+today = now.date()
+tomorrow = now.date() + timedelta(days=1)
+yesterday = now.date() - timedelta(days=1)
 
-timedelta_tonight = price_latest24_earliest_time - now
-timedelta_tomorrow = price_latest24_latest_time - now
+# Then get the power price in desired currency - both for today and tomorrow
+prices_spot = elspot.Prices(currency=base_currency)
+prices_today = prices_spot.hourly(end_date=today, areas=areas)[
+    'areas'][areas[0]]['values']
+prices_tomorrow = prices_spot.hourly(end_date=tomorrow, areas=areas)[
+    'areas'][areas[0]]['values']
 
-if timedelta_tomorrow.days > 0:
-    print("price_latest24_latest_time is for tomorrow night")
-    print("timestamp:", price_latest24_latest_time.time(),
-          "and the price is:", price_latest24_latest)
-elif timedelta_tomorrow.days == 0:
-    print("price_latest24_latest_time is for tonight")
-    print("timestamp:", price_latest24_latest_time.time(),
-          "and the price is:", price_latest24_latest)
-if timedelta_tonight.days > 0:
-    print("price_latest24_earliest_time is for tomorrow night")
-    print("timestamp:", price_latest24_earliest_time.time(),
-          "and the price is:", price_latest24_earliest)
-elif timedelta_tonight.days == 0:
-    print("price_latest24_earliest_time is for tonight")
-    print("timestamp:", price_latest24_earliest_time.time(),
-          "and the price is:", price_latest24_earliest)
 
-print(price_latest24_earliest_time.hour)
-print(price_latest24_earliest_time.date())
-print(price_latest24_latest_time.hour)
-print(price_latest24_latest_time.date())
-print(type(price_latest24_earliest_time))
-print(type(price_latest24_latest_time))
+# Thoughts
+# - ideally, we want tomorrow's prices because it will have both the price of tonight (midnight) and tomorrow night (11pm)
+# - as a backup, we can use today's prices, because it will have the price tonight (11pm) and the useless one: yesterday night (midnight between yesterday and today)
+# -- Conclusion: So there are two interesting prices and one back up price:
+# --- #1: The price tonight -- which is prices_tomorrow[0]
+# --- #2: The price tomorrow night -- which is prices_tomorrow[-1]
+# --- #3: The price tonight (1 hr earlier) -- which is prices_today[-1]
 
-# Is it cheap? If so, when?
-if price_latest24_earliest < cheap_power and price_latest24_earliest < price_latest24_latest:
-    # power is cheapest tonight!
-    print("power is cheapest tonight (and below threshold!), so start charging at:",
-          price_latest24_earliest_time)
-    pass
-elif price_latest24_latest < cheap_power and price_latest24_latest < price_latest24_earliest:
-    # power is cheapest tomorrow!
-    print("power is cheapest tomorrow (and below threshold!), so start charging at:",
-          price_latest24_latest_time)
-    pass
-elif price_latest24_earliest < price_latest24_latest:
-    # power is cheapest tonight, but it's now below the 'cheap' point
-    print("power is cheapest tonight, but not below 'cheap' threshold!")
-    pass
-elif price_latest24_latest < price_latest24_earliest:
-    # power is cheapest tomorrow, but it's now below the 'cheap' point
-    print("power is cheapest tomorrow, but not below 'cheap' threshold!")
-    pass
+# So the logic should be as follows:
+# 1) If the car is set >90% charge limit -> do nothing, we're in Trip Mode™
+# 2) If the car is set to <= 90% charge limit -> decide whether to charge much (max_percent) or little (min_percent)
+# 2a) If the price is below threshold tonight -> set charge to max_percent
+
+if prices_tomorrow[0]['value'] == float('inf'):
+    # tomorrow's prices are not available yet, use the latest price of today instead
+    price_tonight = prices_today[-1]['value']
 else:
-    # it doesn't matter, the price is the same
-    pass
+    # tomorrow's prices are available yet, so use the earliest price
+    price_tonight = prices_tomorrow[0]['value']
 
-# And let's set the charge limit accordingly
+
+# get the car's current charge limit to determine whether it is in Trip Mode™
 if tesla_token:
     c = teslajson.Connection(access_token=tesla_token)
 else:
     c = teslajson.Connection(email=tesla_user, password=tesla_password)
 v = c.vehicles[0]
+current_charge_limit = v.data_request('charge_state')['charge_limit_soc']
+
+if price_tonight < cheap_threshold and current_charge_limit <= 90:
+    print("Wow, that's cheap!", price_tonight)
+    print("We will Max Charge because we're not in trip mode!")
+    command = {"percent": max_percent}
+    print("Sending command with payload:", command)
+    v.command('set_charge_limit', data=command)
+elif current_charge_limit <= 90:
+    print("Okay, not that cheap:", price_tonight)
+    print("We will Min Charge because we're not in trip mode!")
+    command = {"percent": min_percent}
+    v.command('set_charge_limit', data=command)
+    print("Sending command with payload:", command)
+else:
+    print("Hands off! The car is in trip mode!")
+
+# TODO: Do I need these?
+if not any(d['value'] == float('inf') for d in prices_today):
+    # Todays prices has real values
+    pass
+
+if not any(d['value'] == float('inf') for d in prices_tomorrow):
+    # Tomorrows prices has real values
+    pass
 
 # v.wake_up()
 # v.data_request('charge_state')
 # v.command('charge_start')
 
-#w = v.data_request('drive_state')
-w = {'gps_as_of': 1577524888, 'heading': 4, 'latitude': 55.721638, 'longitude': 12.360973, 'native_latitude': 55.721638, 'native_location_supported': 1, 'native_longitude': 12.360973,
-     'native_type': 'wgs', 'power': 0, 'shift_state': None, 'speed': None, 'timestamp': 1577524889351}
+# TODO: Do I want to do something with location?
+# If so need to figure out how to handling rounding of the GPS coordinates
 
-current_latitude = str(w["latitude"])
-current_longitude = str(w["longitude"])
+w = v.data_request('drive_state')
+# w = {'gps_as_of': 1577524888, 'heading': 4, 'latitude': 55.721638, 'longitude': 12.360973, 'native_latitude': 55.721638, 'native_location_supported': 1, 'native_longitude': 12.360973,
+#     'native_type': 'wgs', 'power': 0, 'shift_state': None, 'speed': None, 'timestamp': 1577524889351}
+
+# Seems 3 decimals is imprecise enough that it works for both work locations -- they're far enough apart to not confuse them
+current_latitude = str(round(w["latitude"], 3))
+current_longitude = str(round(w["longitude"], 3))
 
 if current_latitude == home_lat and current_longitude == home_long:
     print("Car is at home.")
