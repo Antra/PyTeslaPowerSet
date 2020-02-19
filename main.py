093d3066131218c3ee009cc9d0498ce6efa986db
Anders Demant van der Weide <antra@antra.dk>
2020-02-19 16:54:34 +0100

    Fix exit logging

--- main.py
+++ main.py
@@ -150,5 +150,4 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
-
-logger.info('*** TeslaPowerSetting ended gracefully ***')
+    logger.info('*** TeslaPowerSetting ended gracefully ***')
